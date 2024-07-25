23af5ba15406bdaccfe1b3cc940e23a285c85d9a
Yevhen <149427191+Yevhen23surfer@users.noreply.github.com>
2024-07-25 10:04:51 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-10 h-[100vh] bg-blue-500 overlow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">
+      <!-- <img
+      width="200"
+      class="mr-3"
+      src="https://vueschool.io/img/logo/vueschool_logo_multicolor_negative.svg"
+      alt="Tre"
+      /> -->
+      My Trello Board
+    </h1>
+    <TrelloBoard />
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
